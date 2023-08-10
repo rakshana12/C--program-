efc52e46ce57c0be7775216677001a029526455c
rakshana12 <131150560+rakshana12@users.noreply.github.com>
2023-08-10 06:50:07 +0530

    Add files via upload

---
+++ circum of circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ double pi=3.14,radius,height,volume ;
+ printf("enter the radius and height");
+ scanf("%lf",& radius);
+ volume=1/3*pi*radius*radius*height;
+ printf("%lf",volume);
+}
